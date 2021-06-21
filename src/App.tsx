e6611e65ecc6d0984bbc6df52604b451aa349389
Blednaya Luna <alexander.shandurenko@gmail.com>
2021-06-22 01:46:29 +0400

    refactor(app): added improved loader and replaced common router to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React, { FC, StrictMode, Suspense } from 'react';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
-import { createBrowserHistory } from 'history';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { CircularProgress } from '@material-ui/core';
+
+import { CircularProgress } from 'components/CircularProgress';
 
 import { routes } from './routes';
-
-const history = createBrowserHistory();
 
 export const App: FC = () => {
   return (
     <StrictMode>
       <ToastContainer />
-      <Router history={history}>
+      <BrowserRouter>
         <Suspense fallback={<CircularProgress />}>
           {renderRoutes(routes)}
         </Suspense>
-      </Router>
+      </BrowserRouter>
     </StrictMode>
   );
 };
---
+++ src/components/CircularProgress/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import {
+  Box,
+  CircularProgress as MuiCircularProgress,
+  Typography,
+} from '@material-ui/core';
+
+import { useStyles } from './styles';
+
+export const CircularProgress: FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <MuiCircularProgress />
+      <Typography className={classes.message}>Loading app...</Typography>
+    </Box>
+  );
+};
