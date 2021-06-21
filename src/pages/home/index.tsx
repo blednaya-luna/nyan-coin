6d5abc11a10efeac6a57787d555588742d8be645
Blednaya Luna <alexander.shandurenko@gmail.com>
2021-06-22 02:01:14 +0400

    refactor(home): a little bit refactored home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,46 @@
 import React, { FC } from 'react';
+import { useHistory } from 'react-router';
+import { Container, Grid, Link, Typography } from '@material-ui/core';
 
 import { Account } from 'features/Account';
 
 const Home: FC = () => {
-  return <Account />;
+  const { push } = useHistory();
+
+  return (
+    <>
+      <Account />
+      <Container maxWidth="sm">
+        <Grid container direction="column" alignItems="flex-start" spacing={1}>
+          <Grid item>
+            <Typography variant="body2">Home page</Typography>
+          </Grid>
+          <Grid item>
+            <Link
+              component="button"
+              variant="body2"
+              onClick={() => {
+                push('/assets');
+              }}
+            >
+              Assets
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link
+              component="button"
+              variant="body2"
+              onClick={() => {
+                push('/admin');
+              }}
+            >
+              Admin
+            </Link>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
